52bf2a29a2ac82dff6eaf47613dc0236a290feae
ian.porras10@gmail.com <ian.porras10@gmail.com>
2024-06-06 13:35:38 -0400

    pushed updated 06/06

--- djangovitual/firstdjangoproject/firstdjangoproject/settings.py
+++ djangovitual/firstdjangoproject/firstdjangoproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstappdjango',
+    'firstappdjango'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,17 @@ WSGI_APPLICATION = 'firstdjangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'mydatabase',   
+        'USER': 'admin',   
+        'PASSWORD': 'password',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
     }
 }
 
